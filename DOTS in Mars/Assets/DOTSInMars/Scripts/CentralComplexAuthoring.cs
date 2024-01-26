a15fbefaf6f3a41027c6ba1c9c81c57ad794258a
Tero Paavolainen <teropaav@hotmail.fi>
2024-01-26 19:58:36 +0200

    feat: unity physics added, started working on first system and authoring

---
+++ DOTS in Mars/Assets/DOTSInMars/Scripts/CentralComplexAuthoring.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Unity.Entities;
+using UnityEngine;
+
+namespace DOTSInMars
+{
+    public class CentralComplexAuthoring : MonoBehaviour
+    {
+
+
+        class CentralComplexBaker : Baker<CentralComplexAuthoring>
+        {
+            public override void Bake(CentralComplexAuthoring authoring)
+            {
+                var entity = GetEntity(TransformUsageFlags.Dynamic);
+                AddComponent(entity, new CentralComplex());
+            }
+        }
+    }
+}
--- DOTS in Mars/Assets/DOTSInMars/Scripts/WorldMovementSystem.cs
+++ DOTS in Mars/Assets/DOTSInMars/Scripts/WorldMovementSystem.cs
@@ -1,18 +1,50 @@
+using DOTSInMars;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.Burst;
+using Unity.Entities;
+using Unity.Jobs;
+using Unity.Jobs.LowLevel.Unsafe;
+using Unity.Mathematics;
+using Unity.Transforms;
 using UnityEngine;
 
-public class WorldMovementSystem : MonoBehaviour
+namespace InMars
 {
-    // Start is called before the first frame update
-    void Start()
+    [BurstCompile]
+    public partial struct WorldMovementSystem : ISystem
     {
-        
+
+        [BurstCompile]
+        public void OnUpdate(ref SystemState state)
+        {
+            var deltaTime = SystemAPI.Time.DeltaTime;
+
+            var centralComplexEntities = SystemAPI.QueryBuilder()
+                .WithAllRW<CentralComplex, LocalTransform>().Build();
+
+            var moveCentral = new MoveCentralComplexEntityJob
+            {
+                DeltaTime = deltaTime,
+                Direction = new float2(1, 0)
+            };
+
+            var moveCentralHandle = moveCentral.ScheduleParallel(centralComplexEntities, state.Dependency);
+
+            state.Dependency = JobHandle.CombineDependencies(state.Dependency, moveCentralHandle);
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    [BurstCompile]
+    public partial struct MoveCentralComplexEntityJob : IJobEntity
     {
-        
+        public float DeltaTime;
+        public float2 Direction;
+
+        private void Execute(in CentralComplex complex, ref LocalTransform transform)
+        {
+            transform.Position.x *= DeltaTime * Direction.x;
+            transform.Position.y *=  DeltaTime * Direction.y;
+        }
     }
 }
