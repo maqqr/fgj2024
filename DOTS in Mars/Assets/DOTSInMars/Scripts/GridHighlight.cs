4fd5728b555b88deb8f2fa4603ca87861fe0ed95
Samuli Kinnunen <samuli.kinnunen@movya.fi>
2024-01-27 23:42:20 +0200

    End of graffagiggels

--- DOTS in Mars/Assets/DOTSInMars/Scripts/GridHighlight.cs
+++ DOTS in Mars/Assets/DOTSInMars/Scripts/GridHighlight.cs
@@ -3,11 +3,30 @@ using System.Collections.Generic;
 using UnityEngine;
 using Unity.Entities;
 using Unity.Mathematics;
+using Unity.Rendering;
 
 namespace DOTSInMars
 {
     public struct GridHighlight : IComponentData
     {
         public float3 Offset;
+    }
+
+    [MaterialProperty("_Offset")]
+    public struct GridHighlightOffset : IComponentData
+    {
+        public float Value;
+    }
+    
+    [MaterialProperty("_Multiplier")]
+    public struct GridHighlightMultiplier : IComponentData
+    {
+        public float Value;
+    }
+    [System.Serializable]
+    [MaterialProperty("_MainColor")]
+    public struct GridHighlightColor : IComponentData
+    {
+        public float4 Value;
     }
 }
